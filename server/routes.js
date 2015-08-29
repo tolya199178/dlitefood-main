9da51a3372029d73d4c4c2d268632631bc08f30c
Kunle <kunle_adetayo@just-fastfood.com>
2015-08-29 23:19:21 +0100

    added routes file

--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,6 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
-  app.use('/api/accounts', require('./'))
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
