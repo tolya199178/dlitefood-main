4de869f5520e1f2b52126790a883f69aadc4684e
anhnt <anhntbk08@gmail.com>
2015-09-04 12:57:49 +0700

    [Restruct the tracker page ]

--- client/app/tracker/tracker.js
+++ client/app/tracker/tracker.js
@@ -4,8 +4,27 @@ angular.module('dLiteMeAdmin')
   .config(function ($stateProvider) {
     $stateProvider
       .state('tracker', {
+        abstract: true,
         url: '/tracker',
         templateUrl: 'app/tracker/tracker.html',
         controller: 'TrackerCtrl'
-      });
+      })
+      .state('tracker.map', {
+        url: '/tracker/map',
+        views: {
+          tracker_map: {
+            templateUrl: "app/tracker/subviews/map.html"
+          }
+        },
+        controller: 'TrackerMapCtrl'
+      })
+      .state('tracker.street', {
+        url: '/tracker/street',
+        views: {
+          tracker_street: {
+            templateUrl: "app/tracker/subviews/street.html"
+          }
+        },
+        controller: 'TrackerStreetCtrl'
+      })
   });
