e67708fe2d84c409e5479585cc3ccecf4ecc5e0a
anhnt <anhntbk08@gmail.com>
2015-09-08 00:08:15 +0700

    [APis] add testing for staff controller

--- server/api/staff/index.js
+++ server/api/staff/index.js
@@ -10,7 +10,7 @@ var router = express.Router();
 router.get('/', auth.hasPermission('STAFF_MANAGEMENT', 'READ'), controller.index);
 // router.get('/activeStaff', auth.hasPermission('STAFF_MANAGEMENT', 'READ'), controller.getActiveStaff);
 router.delete('/:id', auth.hasPermission('STAFF_MANAGEMENT', 'DELETE'), controller.destroy);
-router.put('/updateUserInfo/:id', auth.hasPermission('STAFF_MANAGEMENT', 'UPDATE'), controller.update);
+router.put('/updateStaffInfo/:id', auth.hasPermission('STAFF_MANAGEMENT', 'UPDATE'), controller.update);
 router.post('/', auth.hasPermission('STAFF_MANAGEMENT', 'CREATE'), controller.create);
 // router.put('/updateLocation', auth.isAuthenticated(), controller.updateLocation);
 // router.put('/changeStatus', auth.isAuthenticated(), controller.changeStatus);
